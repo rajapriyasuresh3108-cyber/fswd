44e1f40a16fe2f9343fa3802815d62fc7849e29f
rajapriyasuresh3108-cyber <rajapriyasuresh3108@gmail.com>
2026-09-10 21:07:14 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const authRoutes = require('./routes/auth');
+const todoRoutes = require('./routes/todos');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/todos', todoRoutes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
